e510cb3f87c3fa812d55ab7b223e30d4a8cad47d
sewonkimm <swon962@gmail.com>
2021-02-10 12:55:14 +0900

    feat: 🍎 데일리미션 라우터 추가

---
+++ frontend/src/views/Challenge/DailyMission.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="dailyMission">
+    <p class="participants">
+      명 도전 중
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: "DailyMission",
+};
+</script>
